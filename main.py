67638236a51079da76d0b671f6b1e2a3b3286aab
Micah Cessna <mpcess99@harrisonburg.k12.va.us>
2021-03-09 15:47:35 +0000

    New stuff

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request,render_template
 
-server = Flask(__name__)
+web_site = Flask(__name__)
 
-data = {
-  "text": "hello, world!"
-}
-
-@server.route('/')
+@web_site.route('/', methods=["GET"])
 def index():
-	return jsonify(data)
+	return render_template('index.html')
+@web_site.route('/about')
 
-server.run(host='0.0.0.0', port=8080)
+def about():
+	return render_template('about.html')
+
+web_site.run(host='0.0.0.0', port=8080)
