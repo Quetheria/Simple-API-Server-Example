98c0afcdcd489c46592b70411431c72fb570e0a9
Quetheria <78924156+Quetheria@users.noreply.github.com>
2021-03-18 11:13:01 -0400

    Simple gets

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request,render_template
 
 web_site = Flask(__name__)
+data=[]	
 @web_site.route("/")
 def login():
 	return render_template('login.html')
@@ -8,5 +9,9 @@ def login():
 @web_site.route('/index')
 def index():
 	return render_template('index.html')
+
+@web_site.route("/api/v1.0/send")
+def send_test():
+
 
 web_site.run(host='0.0.0.0', port=8080)	
