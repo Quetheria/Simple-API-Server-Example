02452403f429ebb02a228c48933f8c34eb84809f
Quetheria <78924156+Quetheria@users.noreply.github.com>
2021-03-23 11:50:49 -0400

    Pass to index

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from flask import Flask, jsonify, request,render_template
 
 web_site = Flask(__name__)
-data=[]	
+usernames=[]	
 @web_site.route("/")
 def login():
 	return render_template('login.html')
-
-@web_site.route('/index')
-def index():
-	return render_template('index.html')
 
 @web_site.route("/api/v1.0/send",methods=["POST"])	
 def send_test():
 	username=request.form["username"]
 	return "<h1>text, "+ username+ "!<h1>"
+	usernames.append(username)
+
+@web_site.route('/index',methods=["POST"])
+def index():
+	names=request.form["username"]
+	usernames.append(names)
+	return render_template('index.html',names=usernames)
+
+
 
 web_site.run(host='0.0.0.0', port=8080)	
